60700e6eef9fb5042f643e67785fa6c4c5f111f3
Ninoshka Perez <perez.ninoshka31@gmail.com>
2024-02-02 00:35:27 -0400

    Create 3-square.py

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""This module define a square
+
+"""
+
+
+class Square():
+    """ Define square
+
+    Args:
+        size: the size of the square
+    """
+    def __init__(self, size=0):
+        self.__size = int(size)
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+def area(self):
+        """Method that return the area of the square
+
+        """
+        return self.__size ** 2
