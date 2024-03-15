c88fe2c03dc1418e3d2ab3b74899f2ca5b6aae1a
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-15 11:23:26 -0400

    fix description

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- This scrip creates a table called first_table in the current database in MySQL server
+-- This script creates a table called first_table in the current database in MySQL server
 
-CREATE TABLE IF NOT EXISTS first_table;
+CREATE TABLE IF NOT EXISTS first_table ID INT NAME VARCHAR(256);
