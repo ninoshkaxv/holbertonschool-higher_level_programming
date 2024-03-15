2cc33c7b539c63894c7e1091783bbb7b98c05a49
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-15 13:15:29 -0400

    fix typo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- This script creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiple rows
 
 CREATE TABLE IF NOT EXISTS second_table (
-  ID INT,
-  NAME VARCHAR(256),
-  SCORE INT
+  id INT,
+  name VARCHAR(256),
+  score INT
   );
   INSERT INTO
     second_table
