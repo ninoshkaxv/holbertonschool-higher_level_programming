9f6d14b29891a6f4ce37147abcdda70dded03a3f
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-15 12:06:58 -0400

    fix indentation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,16 @@
 -- This script creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiple rows
 
-CREATE TABLE IF NOT EXISTS second_table;
-INSERT INTO ID INT, NAME VARCHAR(256), SCORE INT
+CREATE TABLE IF NOT EXISTS second_table (
+  ID INT,
+  NAME VARCHAR(256),
+  SCORE INT
+  );
+  INSERT INTO
+    second_table
+  VALUES
+    (1, "Jhon", 10)
+    (2, "Alex", 3)
+    (3, "Bob", 14)
+    (4, "George", 8);
+
+
