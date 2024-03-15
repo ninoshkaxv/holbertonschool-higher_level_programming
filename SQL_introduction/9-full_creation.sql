6ece053f414a5c0e60acc8f551c70632cf5ea4e2
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-15 11:57:35 -0400

    create second_table

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,4 @@
+-- This script creates a table second_table in the database hbtn_0c_0 in MySQL server and add multiple rows
+
+CREATE TABLE IF NOT EXISTS second_table;
+INSERT INTO ID INT, NAME VARCHAR(256), SCORE INT
