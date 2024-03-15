38297b4ae77b7c6b2af6827e98aa28279bd09789
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-15 13:17:53 -0400

    fix typo

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -8,9 +8,9 @@ CREATE TABLE IF NOT EXISTS second_table (
   INSERT INTO
     second_table
   VALUES
-    (1, "Jhon", 10)
-    (2, "Alex", 3)
-    (3, "Bob", 14)
+    (1, "Jhon", 10),
+    (2, "Alex", 3),
+    (3, "Bob", 14),
     (4, "George", 8);
 
 
