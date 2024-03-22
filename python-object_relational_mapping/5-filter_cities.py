43a060a5bc4059ce95a96b4a43dbb192490a4977
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 19:03:25 -0400

    add task 5-14

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+ takes in the name of a state as an argument and lists
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    matchName = sys.argv[4].split(';')[0].strip("'")
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+                JOIN states ON cities.state_id = states.id \
+                WHERE states.name=%s \
+                ORDER BY cities.id ASC", (matchName,)
+                )
+    query_rows = cur.fetchall()
+    cities = []
+    for row in query_rows:
+        cities.append("{}".format(row[0]))
+    print(", ".join(cities))
+    cur.close()
+    conn.close()
---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database
+"""
+
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == '__main__':
+    db_username = sys.argv[1]
+    db_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(db_username, db_password, db_name),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
+    session.close()
