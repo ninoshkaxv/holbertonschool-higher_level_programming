591ffcd36d9ad17585847e4e0688044530a5e58d
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 19:10:01 -0400

    fix pycode

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-""" Script that lists all states with a name starting with N from the database
-hbtn_0e_0_usa
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
-
-    c = serv.cursor()
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = c.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name
+                LIKE BINARY 'N%' ORDER BY states.id""")
+    rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
-            print(row)
-    c.close()
-    serv.close()
+        print(row)
+    cur.close()
+    db.close()
--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-""" Script  that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument.
-"""
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
-
-    serv = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
-
-    c = serv.cursor()
-    stateName = sys.argv[4]
-    c.execute(
-        "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(
-            stateName))
-    rows = c.fetchall()
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                .format(sys.argv[4]))
+    rows = cur.fetchall()
     for row in rows:
-        if row[1] == stateName:
-            print(row)
-    c.close()
-    serv.close()
+        print(row)
+    cur.close()
+    db.close()
