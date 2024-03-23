2f9b42ff7be6b65a67465449618ac5eb160ad250
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 20:46:56 -0400

    fix input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument"""
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+import MySQLdb as sql
+from sys import argv
+if __name__ == '__main__':
+    db = sql.connect(host="localhost",
+                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE\
+                BINARY name = '{}'".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
-    cur.close()
-    db.close()
