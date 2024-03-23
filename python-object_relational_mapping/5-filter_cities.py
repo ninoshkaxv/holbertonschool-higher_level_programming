a4c3383b217a49ab1dba7c170d2452c55c665403
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 21:01:45 -0400

    fix output

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
- takes in the name of a state as an argument and lists
-"""
-
-
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    matchName = sys.argv[4].split(';')[0].strip("'")
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
-                           passwd=password, db=database, charset="utf8")
-    cur = conn.cursor()
-    cur.execute("SELECT cities.name FROM cities \
-                JOIN states ON cities.state_id = states.id \
-                WHERE states.name=%s \
-                ORDER BY cities.id ASC", (matchName,)
-                )
-    query_rows = cur.fetchall()
-    cities = []
-    for row in query_rows:
-        cities.append("{}".format(row[0]))
-    print(", ".join(cities))
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
     cur.close()
-    conn.close()
+    db.close()
