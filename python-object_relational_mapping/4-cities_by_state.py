80cf17bce9ca6a5fe9225e984eee56dd0414ec11
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 20:59:52 -0400

    fix conection

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""script that lists all cities with the state"""
+
+
 import MySQLdb as sql
 from sys import argv
-
-
-if __name__ == "__main__":
-    db = sql.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+if __name__ == '__main__':
+    db = sql.connect(host="localhost", port=3306,
+                     user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.id, cities.name, states.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id""")
+    query = "SELECT cities.id, cities.name,\
+    states.name FROM cities LEFT JOIN states\
+    ON cities.state_id = states.id;"
+    cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
         print(row)
