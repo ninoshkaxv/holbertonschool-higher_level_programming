aa0b2c7760c029472d8779adf1431be44fbf98df
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-03-22 21:03:51 -0400

    fix error on connect

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
-import MySQLdb
-import sys
+"""
+takes in the name of a state as an argument
+and lists all cities of that state
+"""
 
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+import MySQLdb as sql
+from sys import argv
+if __name__ == '__main__':
+    db = sql.connect(host="localhost",
+                     port=3306, user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""", (sys.argv[4],))
+    query = "SELECT cities.name FROM cities JOIN states ON\
+    cities.state_id = states.id WHERE states.name = %s"
+    cur.execute(query, (argv[4],))
     rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
+    cities_array = [row[0] for row in rows]
+    cities_string = ", ".join(cities_array)
+    print(cities_string)
     cur.close()
     db.close()
