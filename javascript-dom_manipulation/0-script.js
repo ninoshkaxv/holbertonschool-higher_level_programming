4a6f29e21760fecd0b91d0dea600cf6612a8d83f
ninoshkaxv <perez.ninoshka31@gmail.com>
2024-05-29 11:29:59 -0400

    add README.md file and update text color

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* Task 0
+  Write a JavaScript script that updates the text color of the header element to red (#FF0000)
+*/
+
+document.querySelector('header').style.color = '#FF0000';
